a70fe7be1e741193e66078a04e781ae3b3aef535
AdithyaSobhin <adithya9eight@gmail.com>
2024-11-03 12:13:59 +0530

    shopping app updated

---
+++ shopping_cart_may-main/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_cart_may/controller/cart_screen_controller.dart';
+import 'package:shopping_cart_may/controller/home_screen_controllerd.dart';
+import 'package:shopping_cart_may/controller/product_detials_screen_controller.dart';
+import 'package:shopping_cart_may/model/cart_model.dart';
+import 'package:shopping_cart_may/view/get_started_screen/get_started_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter(); // hive step 1
+  Hive.registerAdapter(CartModelAdapter());
+  var box = await Hive.openBox<CartModel>("cartBox"); // hive step 1
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => HomeScreenController()),
+        ChangeNotifierProvider(create: (context) => CartScreenController()),
+        ChangeNotifierProvider(
+            create: (context) => ProductDetialsScreenController()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: GetStartedScreen(),
+      ),
+    );
+  }
+}
---
+++ shopping_cart_may-main/lib/view/get_started_screen/get_started_screen.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: prefer_const_constructors, avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shopping_cart_may/view/home_screen/home_screen.dart';
+
+class GetStartedScreen extends StatelessWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+            child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fitHeight,
+                  image: NetworkImage(
+                      "https://images.pexels.com/photos/9218397/pexels-photo-9218397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"))),
+        )),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: InkWell(
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ));
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Get Started",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  SizedBox(width: 10),
+                  Icon(
+                    Icons.arrow_forward_outlined,
+                    color: Colors.white,
+                    size: 30,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
